df1be1b459f7ecfd8e58f310d3653f9e43f18415
ar3ty <ar3tyrt5y@gmail.com>
2025-10-16 19:41:22 +0200

    tokenizing, stopwords, stemming

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,31 +1,43 @@
 import string
 
+from nltk.stem import PorterStemmer
+
 from .search_utils import (
     DEFAULT_SEARCH_LIMIT,
     load_movies,
+    load_stopwords,
 )
 
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
     movies = load_movies()
     results = []
     for movie in movies:
-        preprocessed_query = preprocess_text(query)
-        preprocessed_title = preprocess_text(movie["title"])
-        found = False
-        for title_token in preprocessed_title:
-            for query_token in preprocessed_query:
-                if query_token in title_token:
-                    found = True
-                    results.append(movie)
-                    break
-            if found:
-                found = False
+        query_tokens = tokenize_and_preprocess_text(query)
+        title_tokens = tokenize_and_preprocess_text(movie["title"])
+        if has_matching_token(query_tokens, title_tokens):
+            results.append(movie)
+            if len(results) >= limit:
                 break
-        if len(results) >= limit:
-            break
     return results
 
-def preprocess_text(text: str) -> list[str]:
+def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
+    for title_token in title_tokens:
+        for query_token in query_tokens:
+            if query_token in title_token:
+                return True
+    return False
+
+def fully_matches_to_any(token: str, words: list[str]) -> bool:
+    for word in words:
+        if token == word:
+            return True
+    return False
+
+def tokenize_and_preprocess_text(text: str) -> list[str]:
     text = text.lower()
     text = text.translate(str.maketrans('', '', string.punctuation))
-    return filter(lambda x: "" != x, map(lambda x: x.strip(), text.split()))
+    tokens = list(filter(lambda x: x != "", text.split()))
+    stopwords = load_stopwords()
+    tokens = list(filter(lambda x: not fully_matches_to_any(x, stopwords), tokens))
+    stemmer = PorterStemmer()
+    return list(map(stemmer.stem, tokens))
