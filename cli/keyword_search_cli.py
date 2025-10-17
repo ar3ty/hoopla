9cab54de710f71074528e089f4ddc8891cdbf18b
ar3ty <ar3tyrt5y@gmail.com>
2025-10-17 12:35:38 +0200

    inverted index search

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-import argparse, json
+import argparse
 
-from lib.keyword_search import (
-    search_command,
-)
+from lib.keyword_search import search_command, build_command
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -13,14 +11,21 @@ def main() -> None:
     search_parser = subparsers.add_parser("search", help="Search movies using BM25")
     search_parser.add_argument("query", type=str, help="Search query")
 
+    search_parser = subparsers.add_parser("build", help="Builds the inverted index and saves it to disk")
+
     args = parser.parse_args()
 
     match args.command:
         case "search":
             print(f"Searching for: {args.query}")
-            results = search_command(args.query)
-            for i, res in enumerate(results, 1):
-                print(f"{i}. {res["title"]}")
+            try:
+                results = search_command(args.query)
+                for i, res in enumerate(results, 1):
+                    print(f"{i}. {res["title"]}")
+            except Exception as e:
+                print(f"{e}")
+        case "build":
+            build_command()
         case _:
             parser.exit(2, parser.format_help())
 
--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,23 +1,72 @@
-import string
+import string, os
 
 from nltk.stem import PorterStemmer
+from pickle import dump, load
 
 from .search_utils import (
     DEFAULT_SEARCH_LIMIT,
-    load_movies,
+    PROJECT_ROOT,
     load_stopwords,
+    load_movies,
 )
 
+CACHE_DIR = os.path.join(PROJECT_ROOT, "cache")
+INDEX_PATH = os.path.join(CACHE_DIR, "index.pkl")
+DOCMAP_PATH = os.path.join(CACHE_DIR, "docmap.pkl")
+
+class InvertedIndex:
+    def __init__(self) -> None:
+        self.index = {str: set}
+        self.docmap = {int: dict}
+
+    def __add_document(self, doc_id: int, text: str) -> None:
+        tokens = tokenize_and_preprocess_text(text)
+        for token in tokens:
+            if self.index.get(token) == None:
+                self.index[token] = set()
+            self.index[token].add(doc_id)
+
+    def get_documents(self, term: str) -> list[int]:
+        docs = self.index.get(term, set())
+        return sorted(list(docs))
+
+    def build(self) -> None:
+        items = load_movies()
+        for item in items:
+            item_id = int(item["id"])
+            self.docmap[item_id] = item
+            self.__add_document(item_id, f"{item["title"]} {item["description"]}")
+
+    def save(self) -> None:
+        os.makedirs(CACHE_DIR, exist_ok=True)
+        with open(INDEX_PATH, "wb") as f:
+            dump(self.index, f)
+        with open(DOCMAP_PATH, "wb") as f:
+            dump(self.docmap, f)
+
+    def load(self) -> None:
+        with open(INDEX_PATH, "rb") as f:
+            self.index = load(f)
+        with open(DOCMAP_PATH, "rb") as f:
+            self.docmap = load(f)
+
+def build_command() -> None:
+    idx = InvertedIndex()
+    idx.build()
+    idx.save()
+
 def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT) -> list[dict]:
-    movies = load_movies()
+    idx = InvertedIndex()
+    idx.load()
+    query_tokens = tokenize_and_preprocess_text(query)
+
     results = []
-    for movie in movies:
-        query_tokens = tokenize_and_preprocess_text(query)
-        title_tokens = tokenize_and_preprocess_text(movie["title"])
-        if has_matching_token(query_tokens, title_tokens):
-            results.append(movie)
-            if len(results) >= limit:
-                break
+    for token in query_tokens:
+        indexes = idx.get_documents(token)
+        for index in indexes:
+            results.append(idx.docmap[index])
+            if len(results) >= DEFAULT_SEARCH_LIMIT:
+                return results
     return results
 
 def has_matching_token(query_tokens: list[str], title_tokens: list[str]) -> bool:
